53cb5b09217a2ea20e507e8d8bbaf91838e1f985
Juan Manuel Amador Roa <arroa03@gmail.com>
2024-09-22 21:10:17 -0500

    Change images size

--- components/ui/image-frame.tsx
+++ components/ui/image-frame.tsx
@@ -81,7 +81,7 @@ export const AnimatedTooltip = ({
             width={200}
             src={item.image}
             alt={item.name}
-            className="object-cover !m-0 !p-0 object-top rounded-full h-28 w-28 border-2 group-hover:scale-105 group-hover:z-30 border-white  relative transition duration-500"
+            className="object-cover !m-0 !p-0 object-top rounded-full size-16 border-2 group-hover:scale-105 group-hover:z-30 border-white  relative transition duration-500"
           />
         </motion.div>
       ))}
