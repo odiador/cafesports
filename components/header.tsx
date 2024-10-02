e0e6e6a75ff5d5e3e557032da47217d817b9e466
Juan Manuel Amador Roa <arroa03@gmail.com>
2024-10-02 16:54:59 -0500

    COMO QUE 0 LINEAS DE CODIGO

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,4 @@
-import { IconBrandTwitch, IconBrandInstagram } from "@tabler/icons-react";
+import { IconBrandInstagram } from "@tabler/icons-react";
 import Image from "next/image";
 import Link from "next/link";
 
