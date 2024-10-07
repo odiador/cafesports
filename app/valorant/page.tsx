de7acea2825cf87c423dce8c2df7fe3a2d3d28ec
Juan Manuel Amador Roa <arroa03@gmail.com>
2024-10-07 09:07:11 -0500

    ESLINT te odio

--- app/valorant/page.tsx
+++ app/valorant/page.tsx
@@ -6,34 +6,6 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 
 export default function Valorant() {
-    const renderer = ({ days, hours, minutes, seconds, completed }: { days: number, hours: number, minutes: number, seconds: number, completed: boolean }) => {
-
-        const Cal = ({ round, n, type }: { round?: string, n: number, type?: string }) => {
-            const fst = Number.parseInt((n / 10) + "");
-            const sec = n % 10;
-            return (
-                <div className={`text-white bg-white/5 text-3xl ${round} font-medium w-16 h-16 sm:w-20 sm:h-20 pt-2 px-1 flex flex-col items-center justify-center`}>
-                    <p className="flex text tracking-widest gap-1 justify-center bg-black/0 rounded-lg grow items-center" suppressHydrationWarning>
-                        {`${fst}${sec}`}
-                    </p>
-                    <div className="text-xs font-semibold text-center pb-1">{type}</div>
-                </div>)
-        }
-        if (completed) {
-            days = 0;
-            hours = 0;
-            minutes = 0;
-            seconds = 0;
-        }
-        return (
-            <div className="flex gap-1 rounded-lg w-fit" >
-                <Cal type="días" round="rounded-l-lg" n={days}></Cal>
-                <Cal type="horas" n={hours}></Cal>
-                <Cal type="minutos" n={minutes}></Cal>
-                <Cal round="rounded-r-lg" type="segundos" n={seconds}></Cal>
-            </div>);
-
-    };
     return (
         <motion.div variants={container} initial="hidden" animate="visible" className="flex flex-col gap-4 divide-8 justify-center items-center max-w-3xl h-fit py-8 px-8 rounded-lg bg-black/30 ">
             <motion.div variants={item}>
