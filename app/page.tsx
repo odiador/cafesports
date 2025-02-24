280d399410fca721ba4650208d31bb468f1242f7
Juan Manuel Amador Roa <arroa03@gmail.com>
2025-02-24 08:23:31 -0500

    Change redirect position

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Redirect from "@/components/Redirect";
 export default function Home() {
   return (
     <div className="h-screen flex md:flex-row flex-col-reverse md:p-0 w-full items-center gradient justify-center sm:gap-16">
+      <Redirect />
       <div className="basis">
         <div className="w-fit h-fit flex flex-col items-center">
           <Image src={"/logot.png"} alt={"Logo"} width={408} height={408} className="size-40" />
@@ -13,13 +14,12 @@ export default function Home() {
         </div>
       </div>
       <div className="basis right">
-        PREPARANDO<br />ALGO<br />
+        PREPÁRATE<br />PARA ALGO<br />
         <span className="relative">
           <Highlighting />
           NUEVO
         </span>
       </div>
-      <Redirect />
     </div>
   );
 }
