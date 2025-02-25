f325112455a38e3709ca5deee84858a27ba16f7c
Juan Manuel Amador Roa <arroa03@gmail.com>
2025-02-25 00:59:52 -0500

    fix: change iframe height

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import './page.css';
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col w-full items-center justify-start p-4">
-      <div className="h-svh flex flex-col items-center justify-between gap-8 p-8">
+      <section className="h-svh flex flex-col items-center justify-between gap-8 p-8">
         <div className="w-fit h-fit flex flex-col items-center">
           <Image src={"/logot.png"} alt={"Logo"} width={408} height={408} className="size-40" />
         </div>
@@ -14,17 +14,18 @@ export default function Home() {
           <span className="font-teko text-2xl w-full text-[#a96eb9] text-center">El destino del torneo está en tus manos</span>
         </div>
         <ArrowScroll />
-      </div>
-      <iframe
-        id="form"
-        src="https://docs.google.com/forms/d/e/1FAIpQLSet433DHXMx-59RkCWSDB_L7PK7YzzH6TL6JD_9H8U47QKsFQ/viewform?embedded=true"
-        width="640"
-        height="400"
-        className="w-full max-w-3xl border border-gray-300 bg-white rounded-lg pointer-events-auto"
-      >
-        Cargando…
-      </iframe>
-
+      </section>
+      <section className="h-svh w-full flex flex-col items-center justify-between gap-8 py-4 px-6">
+        <iframe
+          id="form"
+          src="https://docs.google.com/forms/d/e/1FAIpQLSet433DHXMx-59RkCWSDB_L7PK7YzzH6TL6JD_9H8U47QKsFQ/viewform?embedded=true"
+          width="600"
+          height="600"
+          className="w-full h-full max-w-3xl border border-gray-300 bg-white rounded-lg pointer-events-auto"
+        >
+          Cargando…
+        </iframe>
+      </section>
     </div>
   );
 }
