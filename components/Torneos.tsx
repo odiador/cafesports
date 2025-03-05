21978c9554863402e6141dae6a0195341b3b8346
Juan Manuel Amador Roa <arroa03@gmail.com>
2025-03-05 09:48:21 -0500

    Change cards

--- components/Torneos.tsx
+++ components/Torneos.tsx
@@ -8,7 +8,7 @@ import FacebookEmbed from "./embed/FacebookEmbed";
 
 const Torneos = () => {
     return (
-        <div className="mdx-content w-full flex flex-col items-center justify-center gap-1">
+        <div className="pt-0 mdx-content w-full h-full flex flex-col items-center justify-center gap-1">
             <TorneosDM />
         </div >
     )
@@ -16,7 +16,7 @@ const Torneos = () => {
 export const InfoTorneo = () => {
     return (<>
         <div className="flex flex-wrap justify-center items-center w-full gap-4 mt-4">
-            <div className="size-36 bg-white/10 rounded-2xl flex flex-col p-4 items-center justify-center gap-2">
+            <div className="size-36 bg-white/10 rounded-2xl flex flex-col p-3 items-center justify-center gap-2">
                 <h2 ><BsPeopleFill className="text-5xl" /></h2>
                 <div className="flex items-center flex-col justify-center">
                     <div className="flex items-center">
@@ -26,19 +26,19 @@ export const InfoTorneo = () => {
                     <h3 className="font-questrial font-bold text-xl leading-[1.3rem]" >Participantes</h3>
                 </div>
             </div>
-            <div className="size-36 bg-white/10 rounded-2xl flex flex-col p-4 items-center justify-center gap-2">
+            <div className="size-36 bg-white/10 rounded-2xl flex flex-col p-3 items-center justify-center gap-2">
                 <h2><MdLocationPin className="text-5xl" /></h2>
                 <div className="flex flex-col gap-0 items-center">
-                    <h3 className="font-questrial font-bold text-base leading-[1.3rem]" >Uniquindio</h3>
-                    <h3 className="font-questrial font-bold text-sm leading-[1.3rem]" >Sala de Sistemas </h3>
-                    <h3 className="font-questrial font-bold text-xs leading-[1.3rem]" >Bloque de Ingeniería</h3>
+                    <h3 className="font-questrial font-bold text-xs leading-[1rem]" >Uniquindio</h3>
+                    <h3 className="font-questrial font-bold text-xs leading-[1rem]" >Sala de Sistemas </h3>
+                    <h3 className="font-questrial font-bold text-xs leading-[1rem]" >Bloque de Ingeniería</h3>
                 </div>
             </div>
-            <div className="size-36 bg-white/10 rounded-2xl flex flex-col p-4 items-center justify-center gap-2">
+            <div className="size-36 bg-white/10 rounded-2xl flex flex-col p-3 items-center justify-center gap-2">
                 <h2><BiCalendar className="text-5xl" /></h2>
                 <div className="flex flex-col gap-0 items-center">
                     <h3 className="font-questrial font-bold text-sm leading-[1.3rem]" >07 - 11 oct 2024</h3>
-                    <h3 className="font-questrial font-bold text-xs leading-[1.3rem]" >Semana universitaria </h3>
+                    <h3 className="font-questrial font-bold text-xs leading-[1.3rem]" >Semana universitaria</h3>
                 </div>
             </div>
         </div>
