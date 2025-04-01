76b7cb4ceb804e35957ff5d164b5ac2bca5e407a
Juan Manuel Amador Roa <arroa03@gmail.com>
2025-04-01 09:56:01 -0500

    chore: change redirect

--- components/Redirect.tsx
+++ components/Redirect.tsx
@@ -1,19 +1,17 @@
 "use client";
-// import { redirect, RedirectType, usePathname } from "next/navigation";
+import { redirect, RedirectType, usePathname } from "next/navigation";
 import { useEffect } from "react";
 
 
 const Redirect = () => {
-    // const pathname = usePathname();
+    const pathname = usePathname();
 
     useEffect(() => {
-        // const url = window.location.href;
-        // if (!url.startsWith("https://www.cafesports.club/")) {
-        //     redirect(`https://www.cafesports.club${pathname}`, RedirectType.replace);
-        //     return;
-        // }
+        const url = window.location.href;
+        if (!url.startsWith("https://www.cafesports.club/"))
+            redirect(`https://www.cafesports.club${pathname}`, RedirectType.replace);
 
-    }, []);
+    });
 
     return null;
 };
