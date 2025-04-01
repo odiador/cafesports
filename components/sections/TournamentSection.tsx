ba1a4fd38af56ae9f55bb880d7605b1f5c2ebc64
Juan Manuel Amador Roa <arroa03@gmail.com>
2025-04-01 10:46:50 -0500

    fix: change responsive view

--- components/sections/TournamentSection.tsx
+++ components/sections/TournamentSection.tsx
@@ -4,19 +4,19 @@ import Link from "next/link";
 
 export default function TournamentSection({ tournamentInfo }: { tournamentInfo: TournamentInfo }) {
     return (
-        <section className="flex flex-col px-2 py-8 items-center">
-            <div className="relative mb-6">
-                <h1 className="font-bangers">{`Torneo de ${tournamentInfo.name}`}</h1>
-                <div className="absolute text-xl font-nanum -right-16 -bottom-4 -rotate-12 flex flex-col leading-[13px] items-center"><span>
+        <section className="flex flex-col px-2 py-8 items-center w-full">
+            <div className="relative mb-6 flex flex-col">
+                <h1 className="font-bangers px-2">{`Torneo de ${tournamentInfo.name}`}</h1>
+                <div className="place-self-end -rotate-12 text-xl font-nanum flex flex-col leading-[13px] items-center"><span>
                     {`Todos los ${tournamentInfo.date}`}</span>
                     <span className="text-teal-200">11am-2pm</span>
                 </div>
             </div>
-            <div className="flex flex-col max-w-sm sm:max-w-md md:max-w-lg lg:max-w-2xl">
-                <p className="font-questrial">{tournamentInfo.description}</p>
-                <p className="font-questrial font-semibold">{`Siguiente ronda: ${tournamentInfo.activeRound}`}</p>
+            <div className="flex flex-col  max-w-sm sm:max-w-md md:max-w-lg lg:max-w-2xl">
+                <p className="font-questrial text-wrap">{tournamentInfo.description}</p>
+                <p className="font-questrial w-full font-semibold">{`Siguiente ronda: ${tournamentInfo.activeRound}`}</p>
                 <Link className="w-fit place-self-end" href={tournamentInfo.route}>
-                    <button className="rounded-md px-2 hover:scale-110 transition-all py-1 bg-foreground text-black font-teko text-xl">Ver detalles</button>
+                    <button className="rounded-md px-2 hover:scale-110 transition-all py-1 bg-foreground text-black font-questrial font-bold text-lg">Ver detalles</button>
                 </Link>
             </div>
         </section>
